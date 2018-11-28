0521c306b3b7d0a4f374ce086847bf61ef253337
David Podhola <david@podhola.net>
2018-11-28 08:56:29 +0100

    circleci & comp.

--- src/main/java/org/idempiere/common/exceptions/AdempiereException.java
+++ src/main/java/org/idempiere/common/exceptions/AdempiereException.java
@@ -1,9 +1,5 @@
 package org.idempiere.common.exceptions;
 
-import java.util.Properties;
-import org.idempiere.common.util.CLogger;
-import org.idempiere.common.util.Env;
-
 /**
  * Any exception that occurs inside the Adempiere core
  *
@@ -12,11 +8,6 @@ import org.idempiere.common.util.Env;
 public class AdempiereException extends RuntimeException {
   /** */
   private static final long serialVersionUID = 2340179640558569534L;
-
-  /** Default Constructor (saved logger error will be used as message) */
-  public AdempiereException() {
-    this(getMessageFromLogger());
-  }
 
   /** @param message */
   public AdempiereException(String message) {
@@ -40,22 +31,6 @@ public class AdempiereException extends RuntimeException {
   public String getLocalizedMessage() {
     String msg = super.getLocalizedMessage();
     // msg = Msg.parseTranslation(getCtx(), msg); DAP TODO
-    return msg;
-  }
-
-  protected Properties getCtx() {
-    return Env.getCtx();
-  }
-
-  /**
-   * @return error message from logger
-   * @see org.idempiere.common.util.CLogger#retrieveError()
-   */
-  private static String getMessageFromLogger() {
-    org.idempiere.common.util.ValueNamePair err = CLogger.retrieveError();
-    String msg = null;
-    if (err != null) msg = err.getName();
-    if (msg == null) msg = "UnknownError";
     return msg;
   }
 }
